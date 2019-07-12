c967a79781a62340a11af08588f6bf2630a8a091
t3rkecorejz <just-terke@mail.ru>
2019-07-13 01:27:43 +0300

    Добавление массива с пушками

--- php/funcs.php
+++ php/funcs.php
@@ -32,4 +32,23 @@ function loadFromTable($sql_table, $searchId = "id", $searchWord = "") {
     return $count;
   }
 }
-?>
+
+$bitItems = array(
+  array("-1", "Seal Knife"),
+  array("-1", "Граната Напалм"),
+  array("-1", "Граната Заморозка"),
+  array("-1", "Infinity Black"),
+  array("-1", "Winchester M1887"),
+  array("-1", "USAS-12 Camo"),
+  array("0", "JANUS-XI"),
+  array("1", "Coil Gun"),
+  array("-1", "HK-416"),
+  array("-1", "StG 44"),
+  array("1", "Brick Piece V2"),
+  array("0", "JANUS-5"),
+  array("3", "Plasma Gun"),
+  array("-1", "VSK-94 Auto-Sniper"),
+  array("5", "M95 White Tiger"),
+  array("6", "Святая граната"),
+  array("7", "Dread Nova")
+);

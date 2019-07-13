80b9c876f4ddc7bef7d31f600072c80f5e93daac
t3rkecorejz <just-terke@mail.ru>
2019-07-13 03:16:25 +0300

    Новый шрифт + показывает что использует игрок в данный момент

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $data = loadFromTable("zp_weapon_system", $_GET['id']);
     <title>Project X | Profile</title>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="/css/master.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
   </head>
   <body>
@@ -22,6 +22,7 @@ $data = loadFromTable("zp_weapon_system", $_GET['id']);
     <div class="collection">
       <h1>Коллекция</h1>
       <?php foreach ($data as $player) {
+        $primary = $pistol = $knife = $equip = "";
         for($i = 0; $i < count($bitItems); $i++) {
           $classitem = "item_locked";
           if($bitItems[$i][2] == -1 || $bitItems[$i][2] != -1 && $player->bitsum & 1<<$bitItems[$i][2])
@@ -30,7 +31,21 @@ $data = loadFromTable("zp_weapon_system", $_GET['id']);
           echo "<div class='$classitem'>";
             echo "<img style='max-height: 25px;' src='images/items/".$bitItems[$i][3]."' alt='wpn' />", $bitItems[$i][1], "<br />";
           echo "</div>";
+
+          if($player->primary == $bitItems[$i][0]) $primary = $i;
+          if($player->pistol == $bitItems[$i][0]) $pistol = $i;
+          if($player->knife == $bitItems[$i][0]) $knife = $i;
+          if($player->equip == $bitItems[$i][0]) $equip = $i;
         }
+
+        if($primary != -1 || $primary != "")
+          echo "<p>Основное оружие: <i><b>".$bitItems[$primary][1]."</b></i></p>";
+        if($pistol != -1 || $pistol != "")
+          echo "<p>Пистолет: <i><b>".$bitItems[$pistol][1]."</b></i></p>";
+        if($knife != -1 || $knife != "")
+          echo "<p>Нож: <i><b>".$bitItems[$knife][1]."</b></i></p>";
+        if($equip != -1 || $equip != "")
+          echo "<p>Экипировка: <i><b>".$bitItems[$equip][1]."</b></i></p>";
       } ?>
     </div>
   </body>

a4b599237bfaabadde4b632a263330b0c0c4c167
Oleg Deynega <just-terke@mail.ru>
2019-12-01 22:20:38 +0200

    01.12.2019 | Update

--- index.php
+++ index.php
@@ -3,50 +3,68 @@ require_once('php/config.php');
 require_once('php/funcs.php');
 
 $data = loadFromTable("zp_weapon_system", $_GET['id']);
+$player = $data[0];
+$steam_link = SteamID::st32to64($player->steamid);
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <title>Project X | Profile</title>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/darkly/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat&display=swap">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
+    <link rel="stylesheet" href="/css/master.css">
   </head>
   <body>
-    <div class="profile">
-      <div class="steam">
-
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-header">Профиль</div>
+            <div class="card-body">
+              <div class="h3">
+                <?php if($steam_link != null) {
+                  $json = file_get_contents(sprintf("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=3B30DE69513DAE0E4219F25A10E06CC7&steamids=%s", $steam_link));
+                  $parsed = json_decode($json);
+                  $parsed_info = $parsed->response->players[0];
+                  $avatar = $parsed_info->avatarfull;
+                ?>
+                  <a href="https://steamcommunity.com/profiles/<?=$steam_link?>/" target="_blank" title="Steam профиль {name}">
+                    <img src="<?=$avatar?>" alt="avatar" class="img-fluid rounded-circle" style="height: 128px; width: auto;"></a>
+                <?php } ?>
+                Статистика игрока {name}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-12">
+          <div class="card">
+            <div class="card-header">Коллекция</div>
+            <div class="card-body" style="padding: 1px;">
+              <div class="table-responsive">
+                <table class="table table-borderless table-hover table-sm collection">
+                  <thead>
+                    <tr>
+                      <td><center>Изображение</center></td>
+                      <td><center>Название</center></td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php for($i = 0; $i < count($bitItems); $i++) {
+                      if($bitItems[$i][2] == -1 || $bitItems[$i][2] != -1 && $player->bitsum & 1<<$bitItems[$i][2]) {
+                      ?>
+                        <tr>
+                          <td><center><img style="height: 25px;" src="images/items/<?=$bitItems[$i][3]?>" alt="wpn" /></center></td>
+                          <td><span><?=$bitItems[$i][1]?></span></td>
+                        </tr>
+                    <?php } } ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="collection">
-      <h1>Коллекция</h1>
-      <?php foreach ($data as $player) {
-        $primary = $pistol = $knife = $equip = "";
-        for($i = 0; $i < count($bitItems); $i++) {
-          $classitem = "item_locked";
-          if($bitItems[$i][2] == -1 || $bitItems[$i][2] != -1 && $player->bitsum & 1<<$bitItems[$i][2])
-            $classitem = "item_unlocked";
-
-          echo "<div class='$classitem'>";
-            echo "<img style='max-height: 25px;' src='images/items/".$bitItems[$i][3]."' alt='wpn' />", $bitItems[$i][1], "<br />";
-          echo "</div>";
-
-          if($player->primary == $bitItems[$i][0]) $primary = $i;
-          if($player->pistol == $bitItems[$i][0]) $pistol = $i;
-          if($player->knife == $bitItems[$i][0]) $knife = $i;
-          if($player->equip == $bitItems[$i][0]) $equip = $i;
-        }
-
-        if($primary != -1 || $primary != "")
-          echo "<p>Основное оружие: <i><b>".$bitItems[$primary][1]."</b></i></p>";
-        if($pistol != -1 || $pistol != "")
-          echo "<p>Пистолет: <i><b>".$bitItems[$pistol][1]."</b></i></p>";
-        if($knife != -1 || $knife != "")
-          echo "<p>Нож: <i><b>".$bitItems[$knife][1]."</b></i></p>";
-        if($equip != -1 || $equip != "")
-          echo "<p>Экипировка: <i><b>".$bitItems[$equip][1]."</b></i></p>";
-      } ?>
     </div>
   </body>
 </html>
--- php/funcs.php
+++ php/funcs.php
@@ -35,21 +35,40 @@ function loadFromTable($sql_table, $searchId = "id", $searchWord = "") {
 
 $bitItems = array(
   // sysname, itemname, bitsum, image
-  array("KNIFE", "Seal knife", "-1", "Sealknife.png"),
-  array("NAPALM", "Граната Напалм", "-1", "Napalm.png"),
-  array("FROST", "Граната Заморозка", "-1", "Frost.png"),
+
+  // pistols
   array("INFINITYBLACK", "Infinity Black", "-1", "Infinity_black_icon.png"),
+  array("BALROG1", "BALROG-I", "-1", "Balrog1.png"),
+  array("JANUS1", "JANUS-1", "-1", "Janus1.png"),
+
+  // heavy
   array("M1887", "Winchester M1887", "-1", "Icon_m1887_cso.png"),
   array("USAS12", "USAS-12 Camo", "-1",  "Usas12camo.png"),
   array("JANUS11", "JANUS-11", "0", "Janus11.png"),
   array("COILGUN", "Coil Gun", "1", "Coilmg.png"),
+
+  // assault rifles
   array("HK416", "HK-416", "-1", "M4a1hk416.png"),
   array("STG44", "StG 44", "-1", "Stg44.png"),
+  array("M16A4", "M16A4", "-1", "M16a4.png"),
   array("BLOCKAR", "Brick Piece V2", "1", "Blockar_gfx.png"),
   array("JANUS5", "JANUS-5", "0", "Janus-5.png"),
   array("PLASMAGUN", "Plasma Gun", "3", "Plasma.png"),
+
+  // sniper rifles
   array("VSK94", "VSK-94 Auto-Sniper", "-1", "Vsk94.png"),
+  array("CROSSBOW", "Арбалет", "-1", "Crossbow_gfx.png"),
   array("M95TIGER", "M95 White Tiger", "5",  "Buffm95.png"),
+
+  // equipments
+  array("NAPALM", "Граната Напалм", "-1", "Napalm.png"),
+  array("FROST", "Граната Заморозка", "-1", "Frost.png"),
   array("HOLYBOMB", "Святая граната", "6", "Holybomb.png"),
-  array("DREADNOVA", "Dread Nova", "7", "Dreadnova.png")
+  array("CANNONEX", "Red Cannon Dragon", "-1", "Reddragoncannon.png"),
+  array("CLAYMORE", "Claymore Mine MDS", "-1", "claymore.png"),
+
+  // knives
+  array("KNIFE", "Seal knife", "-1", "Sealknife.png"),
+  array("NEWKATANA", "Катана", "-1", "Katana.png"),
+  array("DUALSWORD", "Dual Phantom Slayer", "-1", "Dualswordphantomslayer.png")
 );
